386719b709b1f4360088503a678bad367c4379c9
siew26 <siewyong0726@gmail.com>
2026-02-05 10:33:20 +0000

    move to week1 folder

---
+++ Week1-docker-terraform/terrahomework/variables.tf
@@ -0,0 +1,35 @@
+variable "credentials" {
+  description = "My Credentials"
+  default     = "./keys/my-creds.json"
+}
+
+variable "project" {
+  description = "Project"
+  default     = "terraform-demo-485506"
+}
+
+variable "region" {
+  description = "Region"
+  default     = "us-central1"
+}
+
+variable "location" {
+  description = "Project Location"
+  default     = "US"
+}
+
+variable "bq_dataset_name" {
+  description = "My Bigquery Dataset Name"
+  default     = "demo_dataset"
+}
+
+variable "gcs_bucket_name" {
+  description = "My Storage Bucket Name"
+  default     = "terraform-demo-485506-terra-bucket"
+}
+
+
+variable "gcs_storage_class" {
+  description = "Bucket Storage Class"
+  default     = "STANDARD"
+}
